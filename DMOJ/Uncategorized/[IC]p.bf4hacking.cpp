c951d63896b4d2773246c0f7520f14070f099fd0
dave <d4nplus6@gmail.com>
2025-12-23 14:07:49 -0500

    Learning string algorithms - on hold

---
+++ DMOJ/Uncategorized/[IC]p.bf4hacking.cpp
@@ -0,0 +1,23 @@
+// String Finding - Hacking
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 500000; i++) {
+        cout << "ab";
+    }
+    cout << '\n';
+
+    for (int i = 0; i < 250000; i++) {
+        cout << "ab";
+    }
+    cout << 'a' << '\n';
+
+    return 0;
+}
+
+
+
+// # String Finding - Hacking
+// print("a" * 1000000)
+// print("a" * 499999 + "b")
---
+++ DMOJ/Uncategorized/p.bf4.py
@@ -0,0 +1,7 @@
+# String Finding
+from sys import stdin
+S = stdin.readline().strip()
+T = stdin.readline().strip()
+
+print(S.find(T))
+
