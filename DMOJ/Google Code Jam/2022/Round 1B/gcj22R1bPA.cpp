cbdc70f50ae8e76f58b1c2521c007defd864653b
dave <d4nplus6@gmail.com>
2025-12-26 13:18:36 -0500

    fun 5p google code jam problem

--- DMOJ/Google Code Jam/2022/Round 1B/gcj22R1bPA.cpp
+++ DMOJ/Google Code Jam/2022/Round 1B/gcj22R1bPA.cpp
@@ -2,11 +2,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    
-    
-    
+
+    int T; cin >> T;
+    for (int t = 0; t < T; t++) {
+        int D; cin >> D;
+        deque<int> pc;
+        for (int d = 0; d < D; d++) {int cake; cin >> cake; pc.emplace_back(cake);}
+            if (sizeof(set<int>(pc.begin(),pc.end())) == 1) {printf("Case #%d: %d\n",t+1,D); continue;}
+
+            int res = 0, hold = 0;
+
+            while (!pc.empty()) {
+                int v;
+                if (pc.front() < pc.back()) {v = pc.front(); pc.pop_front();}
+                else {v = pc.back(); pc.pop_back();}
+
+                if (v >= hold) {res++; hold = v;}
+            }
+        printf("Case #%d: %d\n",t+1,res);
+    }
+
     return 0;
 }
