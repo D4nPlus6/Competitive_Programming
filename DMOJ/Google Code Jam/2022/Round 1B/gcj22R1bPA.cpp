d6b65ada7470a53069ada67110f0da28cd739eb6
dave <d4nplus6@gmail.com>
2025-12-25 22:41:37 -0500

    rollback

---
+++ DMOJ/Google Code Jam/2022/Round 1B/gcj22R1bPA.cpp
@@ -0,0 +1,12 @@
+// Google Code Jam '22 Round 1B Problem A - Pancake Deque
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    
+    
+    return 0;
+}
