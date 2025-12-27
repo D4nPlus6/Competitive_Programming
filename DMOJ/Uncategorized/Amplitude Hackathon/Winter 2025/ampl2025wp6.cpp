8b77e97def9246b10514f90e7f001b47d0e11568
dave <d4nplus6@gmail.com>
2025-12-27 08:33:26 -0500

    minor restructuring - organizing uncategorized

---
+++ DMOJ/Uncategorized/Amplitude Hackathon/Winter 2025/ampl2025wp6.cpp
@@ -0,0 +1,7 @@
+// Amplitude Hackathon Winter '25 Problem 6 - 7
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+}
---
+++ DMOJ/Uncategorized/Valentines Day/2019/valentines19j3.cpp
@@ -0,0 +1,9 @@
+// Valentine's Day '19 J3 - Love!
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    return 0;
+}
