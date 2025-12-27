5df3f36ec845ac0e5d56b27543194a4c86785198
dave <d4nplus6@gmail.com>
2025-12-27 16:06:44 -0500

    will finish this later, busy rn

---
+++ DMOJ/Uncategorized/Amplitude Hackathon/Winter 2025/[ic]ampl2025wp6.cpp
@@ -0,0 +1,20 @@
+// Amplitude Hackathon Winter '25 Problem 6 - 7
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    // 🕕️🕖️ (for those who dont get the joke, the clock's are pointing to 6 o clock and 7 o clock respectively)
+    int N; cin >> N;
+    
+
+    int res = 0;
+
+    //🙏
+
+
+
+    cout << res % 67676767 << "\n";
+    return 0;
+}
