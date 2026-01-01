3d24ef36cb174b888165ec4eaedceb6cdff749bb
dave <d4nplus6@gmail.com>
2025-12-31 19:40:36 -0500

    dmoj staff please don't kill me

---
+++ DMOJ/Bulgarian OI/2009/p5/bulgoi09p5.cpp
@@ -0,0 +1,11 @@
+// Bulgarian OI '09 P5 - Special Sequence
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int N; cin >> N;
+    
+    
+    return 0;
+}
