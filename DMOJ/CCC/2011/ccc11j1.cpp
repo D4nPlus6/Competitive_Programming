e0bdad4a44cbd8cb07ada3568579521652fdc66f
david <d4nplus6@gmail.com>
2026-02-16 12:45:22 -0500

    modified CCC '11 J1 C++ sol for CCC grader format

--- DMOJ/CCC/2011/ccc11j1.cpp
+++ DMOJ/CCC/2011/ccc11j1.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    int A,E; cin >> A >> E;
+    int A,E; /* cin >> A >> E; */
+    cout << "How many antennas?\n";
+    cin >> A;
+    cout << "How many eyes?\n";
+    cin >> E;
+
     if (A >= 3 && E <= 4) cout << "TroyMartian\n";
     if (A <= 6 && E >= 2) cout << "VladSaturnian\n";
     if (A <= 2 && E <= 3) cout << "GraemeMercurian\n";
