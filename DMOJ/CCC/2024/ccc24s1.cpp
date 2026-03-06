e5fb0cb629af131213e25fcc86be7cb1182b3061
david <d4nplus6@gmail.com>
2026-03-05 19:06:34 -0500

    removed macros in C++ sol for CCC '24 S1

--- DMOJ/CCC/2024/ccc24s1.cpp
+++ DMOJ/CCC/2024/ccc24s1.cpp
@@ -2,20 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> vi;
-#define endl '\n'
-#define PRIMLOOP(N) for (int i = 1; i <= N; i++)
-
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     int N; cin >> N;
     int dist = N/2;
-    vi table(N+1); PRIMLOOP(N) cin >> table[i];
+    
+    vector<int> table(N+1); 
+    for (int i = 1; i <= N; i++) {
+        cin >> table[i];
+    }
 
     int res = 0;
-    PRIMLOOP(N) {
+    for (int i = 1; i <= N; i++) {
         int target = (i+dist);
         if (table[i] == table[(target <= N) ? target : target%N]) res++;
     }
